f999f0127ad9a4b20a8fbdee82af22dfcadc50f0
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-18 14:50:43 +1000

    parent to child communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ParentComponent } from './child-parent-demo/parent/parent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
